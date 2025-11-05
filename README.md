1771d99091eca03af067006a6a4c753f17734898
Henri Bergius <henri.bergius@iki.fi>
2025-11-05 11:50:24 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+Receive aircraft via ADS-B into Signal K
+========================================
+
+This plugin reads ADS-B aircraft data received using the [dump1090 software](https://github.com/antirez/dump1090) into Signal K.
+
+## Getting started
+
+* Get an [RTL-SDR stick](https://www.rtl-sdr.com/about-rtl-sdr/) with [antenna suitable for 1090MHz](https://lucsmall.com/2017/02/06/making-antennas-for-1090mhz-ads-b-aircraft-tracking/) and plug it into a computer
+* Install and run dump1090 with the `-net` option
+* Install and enable this plugin
+  - If dump1090 is running on a different computer than Signal K, configure the IP address used in the plugin accordingly
---
+++ plugin/index.js
@@ -0,0 +1,113 @@
+module.exports = (app) => {
+  const plugin = {};
+  plugin.id = 'signalk-adsb';
+  plugin.name = 'ADS-B';
+  plugin.description = 'Receive aircraft via ADS-B into Signal K';
+  let interval;
+
+  plugin.start = (settings) => {
+    interval = setInterval(() => {
+      fetch(`http://${settings.host}:${settings.port}/dump1090/data.json`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Request failed with HTTP ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          app.debug(data);
+          if (!data || !data.length) {
+            app.setPluginStatus('Not receiving aircraft via ADS-B');
+            return;
+          }
+          data.forEach((adsb) => {
+            const context = `aircraft.urn:mrn:icao:hex:${adsb.hex}`;
+
+            const values = [
+              {
+                path: '',
+                value: {
+                  name: adsb.flight || adsb.hex,
+                },
+              },
+              {
+                path: 'navigation.position',
+                value: {
+                  latitude: adsb.lat,
+                  longitude: adsb.lon,
+                  altitude: adsb.altitude * 0.3048, // Convert from ft to m
+                },
+              },
+              {
+                path: 'navigation.speedOverGround',
+                value: adsb.speed * 0.5144444, // Convert from kt to m/s
+              },
+              {
+                path: 'navigation.courceOverGround',
+                value: adsb.track * (Math.PI / 180),
+              },
+              {
+                path: 'communication.icao.hex',
+                value: adsb.hex, // Convert from kt to m/s
+              },
+              {
+                path: 'communication.callsignVhf',
+                value: adsb.flight, // Convert from kt to m/s
+              },
+            ];
+            app.handleMessage('signalk-adsb', {
+              context,
+              updates: [
+                {
+                  source: {
+                    label: 'signalk-adsb',
+                    src: `${settings.host}:${settings.port}`,
+                  },
+                  timestamp: new Date().toISOString(),
+                  values,
+                },
+              ],
+            });
+          });
+          app.setPluginStatus(`Receiving ${data.length} aircraft via ADS-B`);
+        })
+        .catch((err) => {
+          app.debug(err);
+          app.setPluginError(err.message);
+        });
+    }, settings.interval * 1000);
+  };
+
+  plugin.stop = () => {
+    if (interval) {
+      clearInterval(interval);
+      interval = undefined;
+    }
+  };
+
+  plugin.schema = {
+    type: 'object',
+    properties: {
+      host: {
+        type: 'string',
+        default: '127.0.0.1',
+        title: 'dump1090 host',
+        description: 'Address of the computer running dump1090',
+      },
+      port: {
+        type: 'integer',
+        default: 8080,
+        title: 'dump1090 port',
+        description: 'Port used for dump1090',
+      },
+      interval: {
+        type: 'integer',
+        default: 10,
+        title: 'Polling interval (in seconds)',
+        description: 'How often to fetch the data from dump1090',
+      },
+    },
+  };
+
+  return plugin;
+};
